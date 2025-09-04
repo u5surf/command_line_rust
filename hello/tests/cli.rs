fc684d6c2edb7389b2b0a28df8b0f8eb07a5754b
u5surf <u5.horie@gmail.com>
2025-09-04 22:21:16 +0900

    init

---
+++ hello/tests/cli.rs
@@ -0,0 +1,13 @@
+use assert_cmd::Command;
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
